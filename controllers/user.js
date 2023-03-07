f7b8b80130ec046ebdde342ddaaab55f3541bd5e
LoicAnael <loicanaelbdf@gmail.com>
2023-03-07 19:32:13 +0400

    configurated authentication token and middleware

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,49 @@
-exports.signup = (req, res, next) => {};
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
-exports.login = (req, res, next) => {};
+exports.signup = (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "utilisateur cree" }))
+        .catch((error) => res.status(404).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+exports.login = (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user === null) {
+        res.status(401).json({ message: "incorrect inputs" });
+      } else {
+        bcrypt
+          .compare(req.body.password, user.password)
+          .then((valid) => {
+            if (!valid) {
+              res.status(401).json({ message: "incorrect inputs" });
+            } else {
+              res.status(200).json({
+                userId: user._id,
+                token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+                  expireIn: "24h",
+                }),
+              });
+            }
+          })
+          .catch((error) => {
+            res.status(500).json({ error });
+          });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+};
